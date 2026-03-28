c45037d71ade2b9f15cd8ddee44c3e59a186e60d
Eugenie Son <2gvmm4xj5n@privaterelay.appleid.com>
2026-03-28 11:19:57 -0600

    Add files via upload

--- a08_pokemon_and_move_classes.py
+++ a08_pokemon_and_move_classes.py
@@ -1,38 +1,16 @@
-# Authors: Hunter Potter, Penina Strong, Eriko Peterson, Eugenie Son
+# Hunter Potter, Penina Strong, Eriko Peterson, Eugenie Son
+# Create Pokemon Class
 
-# Imports
-import random
-
-class Move:
-    def __init__(self, move_name, elemental_type, low_attack_points, high_attack_points):
-        pass  # Penina fills this in
+class Pokemon:
+    def __init__(self, name, elemental_type, hit_points):
+        self.name = name
+        self.elemental_type = elemental_type
+        self.hit_points = hit_points
 
     def get_info(self):
-        pass  # Penina fills this in
+        return f"{self.name} - Type: {self.elemental_type} - Hit Points: {self.hit_points}"
 
-    def generate_attack_value(self):
-        pass  # Penina fills this in
-
-    # I made a basic class Move so I could run my code and make sure it works, the additional attributes still need made for it. --Hunter
-
-
-oTackle = Move("Tackle", "Normal", 5, 20)
-oQuickAttack = Move("Quick Attack", "Normal", 6, 25)
-oSlash = Move("Slash", "Normal", 10, 30)
-oFlamethrower = Move("Flamethrower", "Fire", 5, 30)
-oEmber = Move("Ember", "Fire", 10, 20)
-oWaterGun = Move("Water Gun", "Water", 5, 15)
-oHydroPump = Move("Hydro Pump", "Water", 20, 25)
-oVineWhip = Move("Vine Whip", "Grass", 10, 25)
-oSolarBeam = Move("Solar Beam", "Grass", 18, 27)
-
-MovePool = [oTackle, oQuickAttack, oSlash, oFlamethrower, oEmber, oWaterGun, oHydroPump, oVineWhip, oSolarBeam]
-
-for move in range(3):
-    randomMove = random.choice(MovePool)
-    print(randomMove.get_info())
-    print(f'Generated attack value: {randomMove.generate_attack_value()}')
-    MovePool.remove(randomMove)
-input("Press Enter to continue... ")
-
+    def heal(self):
+        self.hit_points += 15
+        print(f"{self.name} has been healed to {self.hit_points} hit points.")
 
